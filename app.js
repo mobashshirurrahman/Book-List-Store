f599b2a1fd9c1caeab028843fcaab0be60eddd94
Mobashshirur Rahman <mobashshirurrahman@gmail.com>
2020-08-30 00:14:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+// add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  // create tr element
+  const row = document.createElement("tr");
+  //   insert cols
+  row.innerHTML = `
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href="#" class="delete">X</a></td>`;
+  list.appendChild(row);
+};
+// show alert
+UI.prototype.showAlert = function (message, className) {
+  // create div
+  const div = document.createElement("div");
+  // add classes
+  div.className = `alert ${className}`;
+  // add text
+  div.appendChild(document.createTextNode(message));
+  //   get parent
+  const container = document.querySelector(".container");
+  //   get form
+  const form = document.querySelector("#book-form");
+  // insert alert
+  container.insertBefore(div, form);
+  //   timeout after 3sec
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+// delete book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
+// clear fields
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// event listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  //    get form value
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+  //   Instantiate book
+  const book = new Book(title, author, isbn);
+
+  //   instantiate UI
+  const ui = new UI();
+
+  //   validate
+  if (title === "" || author === "" || isbn === "") {
+    //   error alert
+    ui.showAlert("Plz fill all fields", "error");
+  } else {
+    //   Add Book to list
+    ui.addBookToList(book);
+
+    // show sucess
+    ui.showAlert("book added!", "success");
+    //   clear fields
+
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // ui instantiate
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+  // show message
+
+  ui.showAlert("Book removed", "success");
+  e.preventDefault();
+});
